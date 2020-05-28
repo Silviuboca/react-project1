ce98d1cabe597eb9a4156a95445ae4f69c824655
Silviu Bocancea <silviuboc4@gmail.com>
2020-05-28 14:52:08 -0700

    Updates

---
+++ precache-manifest.0f2a9b371eec9c4d6b3bb13e52356be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aef5c018c3b58375a85b3783691d607",
+    "url": "/react-project1/index.html"
+  },
+  {
+    "revision": "1f39e046b6a90d3387f6",
+    "url": "/react-project1/static/css/main.42873ff0.chunk.css"
+  },
+  {
+    "revision": "c9d245908695d103219a",
+    "url": "/react-project1/static/js/2.74b43fab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project1/static/js/2.74b43fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f39e046b6a90d3387f6",
+    "url": "/react-project1/static/js/main.c31d6f82.chunk.js"
+  },
+  {
+    "revision": "fd947cce23a5148f3fc3",
+    "url": "/react-project1/static/js/runtime-main.d0dbc8aa.js"
+  }
+]);
